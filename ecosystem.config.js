abe5d29f0829642689bc2598ad73cffeded93735
WoodyWangHou <wanghou92@gmail.com>
2017-05-29 21:59:20 +0800

    edit eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:WoodyWangHou/test_NUSAutograder_server.git',
       path : 'test-server',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && sudo pm2 reload ecosystem.config.js --env production'
     }
   }
 };
