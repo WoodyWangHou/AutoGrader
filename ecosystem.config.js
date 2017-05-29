5ed48ab5ca2620746c2eb4f437db8f81b24fa629
WoodyWangHou <wanghou92@gmail.com>
2017-05-29 21:44:50 +0800

    edit eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,11 +19,11 @@ module.exports = {
   deploy : {
     production : {
       user : 'ubuntu',
-      host : 'ec2-54-169-84-78.ap-southeast-1.compute.amazonaws.com',
-      // key: '~/.ssh/test-nusautograder.pem',
+      host : 'ec2-52-77-231-105.ap-southeast-1.compute.amazonaws.com',
+      key  : '~/.ssh/test-nusautograder.pem',
       ref  : 'test/master',
       repo : 'git@github.com:WoodyWangHou/test_NUSAutograder_server.git',
-      path : '/test-server',
+      path : 'test-server',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
